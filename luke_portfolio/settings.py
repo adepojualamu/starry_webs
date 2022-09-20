cc074ff901424d69ff2bccf76ee40f5e5db8d420
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2022-09-21 00:11:39 +0400

    fixed db not seeing debug=false from local_settings

--- luke_portfolio/settings.py
+++ luke_portfolio/settings.py
@@ -70,30 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'luke_portfolio.wsgi.application'
 
-
-# Database
-
-if DEBUG:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': os.getenv('DB_ENGINE'),
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASS'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': int(os.getenv('DB_PORT')),
-        }
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -166,3 +142,27 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
+# Database
+
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASS'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': int(os.getenv('DB_PORT')),
+        }
+    }
